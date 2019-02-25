6733582422e15f4de6a142bb392f6be63bad2409
Hernán Bs <bort05@hotmail.com>
2019-02-25 13:06:23 -0300

    PaymentDetail API Created

---
+++ WebApiAndAngular/Models/PaymentDetailContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAndAngular.Models
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetais { get; set; }
+    }
+}
